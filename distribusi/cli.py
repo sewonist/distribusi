dbe0e59b908f2d8159983348d25a8274eaf467f8
rra <rscmbbn@riseup.net>
2019-09-04 16:11:28 +0200

    clean up argparse

--- distribusi/cli.py
+++ distribusi/cli.py
@@ -4,22 +4,22 @@ from distribusi.distribusi import distribusify
 
 
 def build_argparser():
-    parser = argparse.ArgumentParser(
-        """
+    parser = argparse.ArgumentParser("""
     distbusi is a content management system for the web that produces static
-    index pages based on folders in the filesystem. It is inspired by the
-    automatic index functions featured in several web servers. It works by
+    index pages based on folders in the files system. It is inspired by the 
+    automatic index functions featured in several popular web servers. It works by
     traversing the file system and directory hierarchy to automatically list
-    all the files in the directory and providing them with html classes and
-    tags for easy styling.
-    """
+    all the files in the directory, detect the file types and providing them with 
+    relevant html classes and tags for easy styling."""
     )
 
     parser.add_argument(
         '-d', '--directory', help="Select which directory to distribute"
     )
 
-    parser.add_argument('-s', '--style', help="Select a CSS style to include")
+    parser.add_argument(
+        '-s', '--style', help="Select a CSS style sheet to include"
+    )
 
     parser.add_argument(
         '-v', '--verbose', help="Print verbose debug output", action="store_true"
@@ -28,7 +28,7 @@ def build_argparser():
     parser.add_argument(
         '-t',
         '--thumbnail',
-        help="Generate 150x150 thumbnails for images",
+        help="Generate 450x450 thumbnails for images",
         action="store_true",
     )
 
@@ -42,7 +42,7 @@ def build_argparser():
     parser.add_argument(
         '-nf',
         '--no-filenames',
-        help="Don't include image filenames",
+        help="Don't include image file names",
         action="store_true",
     )
 
