bc6fa0bad87fecedf395b27268a871aea74e1e08
rra <rscmbbn@riseup.net>
2019-09-12 18:46:33 +0200

    unknown files now have icon

--- distribusi/page_template.py
+++ distribusi/page_template.py
@@ -2,14 +2,16 @@ html_head = """
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Generated with distribusi https://git.vvvvvvaria.org/rra/distribusi -->
+    <!-- Generated with distribusi https://git.vvvvvvaria.org/varia/distribusi -->
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <style>
       .image{max-width: 100%%;}
-      .pdf {width:100%%;}
+      .pdf object{width:100%%;}
+      .dir::before{content:"📁 ";font-size:18px;}
+      .filename{display:block;}
+      .unkown-file::before{content:"📄 ";font-size:18px;}
       div{width: 640px;float:left;padding:1em;}
       video {width:640px;}
-      .dir::before{content:"📁";font-size:18px;}
       %s
     </style>
   </head>
